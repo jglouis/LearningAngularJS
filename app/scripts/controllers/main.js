5b162d999511afa67ceeeb92cbc1924cb991549b
Jean-Guillaume Louis <jguillame.louis@gmail.com>
2016-02-02 22:13:05 +0100

    Add Yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name learningAngularJsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the learningAngularJsApp
+ */
+angular.module('learningAngularJsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
