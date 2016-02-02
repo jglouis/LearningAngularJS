69a08557f8900431907acdb55f4c94f770ef43ad
Jean-Guillaume Louis <jguillame.louis@gmail.com>
2016-02-02 22:26:29 +0100

    Add basic TODO list with Add and Remove functionalities

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,14 @@
  * # MainCtrl
  * Controller of the learningAngularJsApp
  */
-angular.module('learningAngularJsApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+ angular.module('learningAngularJsApp')
+   .controller('MainCtrl', function ($scope) {
+     $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
+     $scope.addTodo = function () {
+       $scope.todos.push($scope.todo);
+       $scope.todo = '';
+     };
+     $scope.removeTodo = function (index) {
+      $scope.todos.splice(index, 1);
+    };
+   });
