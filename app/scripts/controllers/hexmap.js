7321137b1934bcdeaa950582589ecd147f6c67e6
Jean-Guillaume Louis <jguillame.louis@gmail.com>
2016-02-06 18:54:22 +0100

    Display map with axial coordinates
    Use axial coordinates for creation of the grid. Need to rework the
    "onClick" callback to take that into account.

--- app/scripts/controllers/hexmap.js
+++ app/scripts/controllers/hexmap.js
@@ -17,7 +17,6 @@ angular.module('learningAngularJsApp')
   });
 
   // Hex math defined here: http://blog.ruslans.com/2011/02/hexagonal-grid-math.html
-
   function HexagonGrid(canvasId, radius) {
       this.radius = radius;
 
@@ -31,49 +30,42 @@ angular.module('learningAngularJsApp')
       this.canvasOriginX = 0;
       this.canvasOriginY = 0;
 
-      this.canvas.addEventListener("mousedown", this.clickEvent.bind(this), false);
-  };
+      this.canvas.addEventListener('mousedown', this.clickEvent.bind(this), false);
+  }
 
-  HexagonGrid.prototype.drawHexGrid = function (rows, cols, originX, originY, isDebug) {
+  HexagonGrid.prototype.drawHexGrid = function (radius, originX, originY) {
       this.canvasOriginX = originX;
       this.canvasOriginY = originY;
 
-      var currentHexX;
-      var currentHexY;
-      var debugText = "";
+      for (var u = -radius; u < radius; u++){
+        for (var v = -radius; v < radius; v++){
+          var a = {u: u, v: v};
+          var b = {u: 0, v: 0};
 
-      var offsetColumn = false;
-
-      for (var col = 0; col < cols; col++) {
-          for (var row = 0; row < rows; row++) {
-
-              if (!offsetColumn) {
-                  currentHexX = (col * this.side) + originX;
-                  currentHexY = (row * this.height) + originY;
-              } else {
-                  currentHexX = col * this.side + originX;
-                  currentHexY = (row * this.height) + originY + (this.height * 0.5);
-              }
-
-              if (isDebug) {
-                  debugText = col + "," + row;
-              }
-
-              this.drawHex(currentHexX, currentHexY, "#ddd", debugText);
+          if (this.Distance(a,b) > 3){
+            continue;
           }
-          offsetColumn = !offsetColumn;
+
+          var y = this.radius * Math.sqrt(3) * (u + v/2);
+          var x = this.radius * 3/2 * v;
+
+          x += originX;
+          y += originY;
+
+          this.drawHex(x, y, '#ddd', '(' + u + ',' + v + ')');
+        }
       }
   };
 
   HexagonGrid.prototype.drawHexAtColRow = function(column, row, color) {
-      var drawy = column % 2 == 0 ? (row * this.height) + this.canvasOriginY : (row * this.height) + this.canvasOriginY + (this.height / 2);
+      var drawy = column % 2 === 0 ? (row * this.height) + this.canvasOriginY : (row * this.height) + this.canvasOriginY + (this.height / 2);
       var drawx = (column * this.side) + this.canvasOriginX;
 
-      this.drawHex(drawx, drawy, color, "");
+      this.drawHex(drawx, drawy, color, '');
   };
 
   HexagonGrid.prototype.drawHex = function(x0, y0, fillColor, debugText) {
-      this.context.strokeStyle = "#000";
+      this.context.strokeStyle = '#000';
       this.context.beginPath();
       this.context.moveTo(x0 + this.width - this.side, y0);
       this.context.lineTo(x0 + this.side, y0);
@@ -91,8 +83,8 @@ angular.module('learningAngularJsApp')
       this.context.stroke();
 
       if (debugText) {
-          this.context.font = "8px";
-          this.context.fillStyle = "#000";
+          this.context.font = '8px';
+          this.context.fillStyle = '#000';
           this.context.fillText(debugText, x0 + (this.width / 2) - (this.width/4), y0 + (this.height - 5));
       }
   };
@@ -105,11 +97,11 @@ angular.module('learningAngularJsApp')
           do {
               x += layoutElement.offsetLeft;
               y += layoutElement.offsetTop;
-          } while (layoutElement = layoutElement.offsetParent);
+          } while (layoutElement === layoutElement.offsetParent);
 
           return { x: x, y: y };
       }
-  }
+  };
 
   //Uses a grid overlay algorithm to determine hexagon location
   //Left edge of grid has a test to acuratly determin correct hex
@@ -122,8 +114,7 @@ angular.module('learningAngularJsApp')
 
       var column = Math.floor((mouseX) / this.side);
       var row = Math.floor(
-          column % 2 == 0
-              ? Math.floor((mouseY) / this.height)
+          column % 2 === 0 ? Math.floor((mouseY) / this.height)
               : Math.floor(((mouseY + (this.height * 0.5)) / this.height)) - 1);
 
 
@@ -133,48 +124,47 @@ angular.module('learningAngularJsApp')
 
           //Now test which of the two triangles we are in
           //Top left triangle points
-          var p1 = new Object();
+          var p1 = {};
           p1.x = column * this.side;
-          p1.y = column % 2 == 0
-              ? row * this.height
+          p1.y = column % 2 === 0 ? row * this.height
               : (row * this.height) + (this.height / 2);
 
-          var p2 = new Object();
+          var p2 = {};
           p2.x = p1.x;
           p2.y = p1.y + (this.height / 2);
 
-          var p3 = new Object();
+          var p3 = {};
           p3.x = p1.x + this.width - this.side;
           p3.y = p1.y;
 
-          var mousePoint = new Object();
+          var mousePoint = {};
           mousePoint.x = mouseX;
           mousePoint.y = mouseY;
 
           if (this.isPointInTriangle(mousePoint, p1, p2, p3)) {
               column--;
 
-              if (column % 2 != 0) {
+              if (column % 2 !== 0) {
                   row--;
               }
           }
 
           //Bottom left triangle points
-          var p4 = new Object();
+          var p4 = {};
           p4 = p2;
 
-          var p5 = new Object();
+          var p5 = {};
           p5.x = p4.x;
           p5.y = p4.y + (this.height / 2);
 
-          var p6 = new Object();
+          var p6 = {};
           p6.x = p5.x + (this.width - this.side);
           p6.y = p5.y;
 
           if (this.isPointInTriangle(mousePoint, p4, p5, p6)) {
               column--;
 
-              if (column % 2 == 0) {
+              if (column % 2 === 0) {
                   row++;
               }
           }
@@ -196,7 +186,7 @@ angular.module('learningAngularJsApp')
       b2 = this.sign(pt, v2, v3) < 0.0;
       b3 = this.sign(pt, v3, v1) < 0.0;
 
-      return ((b1 == b2) && (b2 == b3));
+      return ((b1 === b2) && (b2 === b3));
   };
 
   HexagonGrid.prototype.clickEvent = function (e) {
@@ -208,9 +198,13 @@ angular.module('learningAngularJsApp')
 
       var tile = this.getSelectedTile(localX, localY);
       if (tile.column >= 0 && tile.row >= 0) {
-          var drawy = tile.column % 2 == 0 ? (tile.row * this.height) + this.canvasOriginY + 6 : (tile.row * this.height) + this.canvasOriginY + 6 + (this.height / 2);
+          var drawy = tile.column % 2 === 0 ? (tile.row * this.height) + this.canvasOriginY + 6 : (tile.row * this.height) + this.canvasOriginY + 6 + (this.height / 2);
           var drawx = (tile.column * this.side) + this.canvasOriginX;
 
-          this.drawHex(drawx, drawy - 6, "rgba(110,110,70,0.3)", "");
+          this.drawHex(drawx, drawy - 6, 'rgba(110,110,70,0.3)', '');
       }
+  };
+
+  HexagonGrid.prototype.Distance = function(a, b){
+    return (Math.abs(a.u - b.u) + Math.abs(a.u + a.v - b.u - b.v) + Math.abs(a.v - b.v)) / 2;
   };
